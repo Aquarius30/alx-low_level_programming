24180e996f40d95ad4a5f0f78266b0d43792740f
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-13 13:58:33 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after cde succession
+ */
+int main(void)
+{
+int digit;
+for (digit = 10; digit < 10; digit++)
+{
+printf("%digit", digit);
+}
+printf("\n");
+return (0);
+}
