812eab1b68970ff2e6d7457a70dfd708847e35dc
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-13 07:37:47 -0700

    bonus 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Return 1 success
+ *bonus. printf task
+ * Return 1 after succesful execution
  */
 int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
