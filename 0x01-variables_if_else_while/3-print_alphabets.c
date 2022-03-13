638aedae6e16de7737d07e617f31100c08d6ead9
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-13 13:29:19 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,11 @@ int main(void)
 {
 char uppercase;
 char lowercase;
-for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+for (uppercase = 'a'; uppercase <= 'z'; uppercase++)
 {
 putchar(uppercase);
 }
-for (lowercase = 'a'; lowercase<= 'z'; lowercase++)
+for (lowercase = 'A'; lowercase <= 'Z'; lowercase++)
 {
 putchar(lowercase);
 }
