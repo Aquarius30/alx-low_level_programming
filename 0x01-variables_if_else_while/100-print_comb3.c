6d38cfe548855e95db4e0db77537f389ccf49abe
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-14 04:51:54 -0700

    bonuss

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,15 @@
+#include <sstdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after code succession
+ */
+int main(void)
+{
+int comb
+for (comb = 1; comb <= 9; comb++)
+{
+putchar('%x', comb);
+}
+return (0);
+}
