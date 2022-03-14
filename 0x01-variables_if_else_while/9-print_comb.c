fa916f76103e5bfa30af0068f560c5edd5e7612b
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-14 04:40:34 -0700

    separation

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int number;
 for (number = 40; number <= 57; number++)
 {
 putchar(number);
-}
 if (number == 57)
 {
 break;
 }
+}
 putchar(',');
-putchar(' ')';
+putchar(' ');
 putchar('\n');
 return (0);
 }
