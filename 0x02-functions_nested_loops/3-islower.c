c4d092769df5110d1f4dcff6beba82a356b35fa3
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-16 01:52:17 +0300

    lower or upper

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - checks if letter is lower or uppercase
- *
+ * @c: takes in character
  * Return: 0 after code completion
  */
 int _islower(int c)
@@ -9,7 +9,8 @@ int _islower(int c)
 if (c >= 'a' && c <= 'z')
 {
 return (1);
-}else
+} 
+else
 {
 return (0);
 }
