13d1f44501ede313693681a3724282dc0f029d39
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-15 23:26:27 +0300

    function alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * void - no return function
+ *
+ * Return: 0 after code succession
+ */
+void print_alphabet(void);
+{
+char i;
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
