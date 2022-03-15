c2d77eb2446f727a0bd8e5f60369b7d65c04dbe7
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-16 01:41:48 +0300

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 int number;
 char alphabet;
-for (number = 1; number <=10; number++)
+for (number = 1; number <= 10; number++)
 {
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 _putchar(alphabet);
