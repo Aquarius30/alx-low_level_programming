ccde3a43ad12efcb6d87fbc8617078129fe9a81a
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-16 01:27:49 +0300

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * void - prints out alphabets
+ *
+ * Return: 0 after code succession
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c < 'z'; c++)
+{
+_putchar(c);
+}
+putchar('\n');
+}
