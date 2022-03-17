39cb675fdef8c726d5ea9757f333b29c78d9a6aa
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-18 01:44:37 +0300

    print 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10x the numbers
+ */
+void more_numbers(void);
+{
+	int i;
+	int z;
+
+	for (i > 0; i <= 10; i++)
+	{
+		z = 0;
+		while (z < 15)
+		{
+			_putchar(z + '0');
+		}
+		putchar('\n');
+	}
+}
+
+		
