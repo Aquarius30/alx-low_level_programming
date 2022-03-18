027115421a23f5dcfaaf991c264c0f4cef0e2b54
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-19 00:01:00 +0300

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - prints square
+ * @size: takes int
+ */
+void print_square(int size)
+{
+	int i;
+	int b;
+
+	if (n > 0)
+	{
+		i = 0;
+		while (i < n)
+		{
+			for (b = 0; b < i; b++)
+			{
+				_putchar(35);
+			}
+			i++;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
