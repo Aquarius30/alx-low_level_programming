c5c98590acd42801dfa5f1caddde8752a518ac3d
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-22 23:55:25 +0300

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,12 +13,13 @@ void print_array(int *a, int n)
 	while (i < n)
 	{
 		printf("%d", a[i]);
-	if (i < n - 1)
+
+		if (i < n - 1)
 	{
 		printf(", ");
 	}
 	i++;
-	printf("\n");
 	}
+	printf("\n");
 }
 			
