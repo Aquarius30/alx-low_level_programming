d7e6adea880f503fef24c4133b4ebf0df343bcf3
aquarius30 <yohannesabiti3094@gmail.com>
2022-03-22 22:42:32 +0300

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: int
+ * Return: u
+ */
+int _strlen(char *s)
+{
+	int x;
+	u = 0;
+	while (s[u] != "\o")
+	{
+		u++;
+	}
+	return (u);
+}
