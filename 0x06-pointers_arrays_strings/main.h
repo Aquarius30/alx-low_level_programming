05370ad49c4b2f29fadc95337354e39cfaa9b617
aquazod <yohannesabiti3094@gmail.com>
2022-03-24 21:38:08 +0300

    task 1

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+char *_strcat(cat *dest, char *src);
+char *strncat(char *dest, char *src, int n);
+char *strncpy(char *dest, char *src, int n);
+int _strcmp(char *sr1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap-string(char *;
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
